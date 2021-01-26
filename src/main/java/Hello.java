7578bd15a4adfad88b58908600c55e970cd764b3
Anna Shabalina <shabalina@gmail.com>
2021-01-26 15:39:37 -0500

    Master branch text is added. Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, test, test");
         System.out.println("2021 is the year of B21, future SDET");
     }
 }
