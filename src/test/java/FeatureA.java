e2f8065fab9afab2312814ad4d2a1c5f985e2378
Anna Shabalina <shabalina@gmail.com>
2021-01-26 14:24:09 -0500

    Anna's 5th added new Branch "feature-a" from 4th class

---
+++ src/test/java/FeatureA.java
@@ -0,0 +1,6 @@
+public class FeatureA {
+    public static void main(String[] args) {
+        System.out.println("Awesome work @featureA");
+        System.out.println("More work around");
+    }
+}
